16dd3b2aa0bb2a645ac0ceb1cd064583b468464e
nishant-thapa <np05cp4a230120@iic.edu.np>
2024-08-18 13:27:35 +0545

    ways of extracting data from the database

--- myApp/views.py
+++ myApp/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render, redirect
+from .models import MyAppDB
 # Create your views here.
 
 def home_page(request):
-    return render(request,'index.html')
+    data=MyAppDB.objects.first()
+    return render(request,'index.html',{'data':data})
+
+# Ways of getting data from the database;
+
+     # first()
+# obj = YourModelName.objects.filter(age__gt=18).first()
+
+
+     # all()
+# objects = YourModelName.objects.all()
+
+
+      # get()
+# obj = YourModelName.objects.get(name='Nishant')
+
+
+    # filter()----------------------------------
+# MyAppDB.objects.filter(age__gt=18)
+# __lt: Less than
+# __lte: Less than or equal to
+# __gte: Greater than or equal to
+# __exact: Exactly equal to
+# __contains: Contains (for text fields)
+
